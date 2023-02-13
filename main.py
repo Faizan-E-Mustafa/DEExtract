dcecebe5c2ba7523f1e382017dd179fafd1ab330
Faizan E Mustafa <femustafa@lab.local>
2023-02-13 19:29:15 +0100

    * break into clauses using conjuction only

--- main.py
+++ main.py
@@ -1,46 +1,91 @@
 #  tags explanation:  https://www.sketchengine.eu/german-stts-part-of-speech-tagset/
 import spacy
-from spacy.lang.de.examples import sentences
 
-nlp = spacy.load("de_core_news_sm")
-doc = nlp("Ich hatte mich gewaschen")
-print(doc.text)
-for token in doc:
-    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_)
-    if token.pos_ != "VERB":
-        continue
 
-    if token.tag_ == "VVINF":  # infinitive form
-        print("infinitive")
-        print(token.text)
-        continue
+class VocabExtractor:
+    def __init__(self) -> None:
+        pass
 
-    if token.tag_ in [
-        "VVIZU",  #  Infinitiv mit "zu"e.g anzukommen
-        "VVPP",  # past participle of full verb e.g gekommt
-    ]:
-        print("Infinitiv mit zu")
-        print(token.lemma_)
-        continue
-
-    # import pdb; pdb.set_trace()
-    if token.tag_ == "VVFIN":
-        separable_verb_conjugated = ""
+    def check_reflexive_verb(self, token):
         reflexive = ""
 
-        for child in token.children:
-            if (
-                child.pos_ == "ADP" and child.tag_ == "PTKVZ"
-            ):  # separable verb conjugated
-                separable_verb_conjugated = child.text + token.text
+        if token.tag_ in [
+            "VVIZU",  #  Infinitiv mit "zu"e.g anzukommen
+            "VVPP",  # past participle of full verb e.g gekommt
+            "VVFIN",  # conjugated
+        ]:
+            for child in token.children:
+                if child.tag_ == "PRF":
+                    reflexive = f"sich {token.lemma_}"
+                    break
+        return reflexive
 
-            if child.tag_ == "PRF":
-                reflexive = f"sich {token.lemma_}"
-                # print(reflexive)
+    def check_separable_verb(self, token):
+        separable = ""
 
-        if separable_verb_conjugated or reflexive:
-            print(separable_verb_conjugated)
-            print(reflexive)
-        else:  # conjugated
-            print("conjugated")
-            print(token.lemma_)
+        if token.tag_ in ["VVFIN"]:  # conjugated
+            for child in token.children:
+                if (
+                    child.pos_ == "ADP" and child.tag_ == "PTKVZ"
+                ):  # separable verb conjugated
+                    separable = child.text + token.text
+        return separable
+
+    def extract_verbs(self, sentence):
+        for token in sentence:
+            if token.pos_ != "VERB":
+                continue
+
+            # check infinitive form
+            if token.tag_ == "VVINF":
+                verb_found = token.text
+                return verb_found
+
+            verb_found = self.check_reflexive_verb(token)
+            if verb_found:
+                return verb_found
+
+            verb_found = self.check_separable_verb(token)
+            if verb_found:
+                return verb_found
+
+            #  conjugated: should not be placed before reflexive and separable
+            if token.tag_ in ["VVIZU", "VVPP", "VVFIN"]:
+                verb_found = token.lemma_
+                return verb_found
+
+
+
+nlp = spacy.load("de_core_news_sm")
+doc = nlp(
+    "Ich hoffe, dass ich rechtzeitig anzukommen werde, um den Anfang der Konferenz nicht zu verpassen"
+)
+
+doc = nlp("Sie hat sich gestern Abend ausgespannt")
+
+{   "KOKOM":"comparative conjunction",
+    "KON": "coordinate conjunction",
+    "KOUI": 'subordinate conjunction with "zu" and infinitive',
+    "KOUS": "subordinate conjunction with sentence"}
+
+clauses = []
+for sent in doc.sents:
+    clause_start_idx = 0
+    for clause_end_idx, token in enumerate(sent):
+       
+        if token.tag_ in ["KOKOM", "KON", "KOUI", "KOUS"]:
+            
+            clauses.append(sent[clause_start_idx:clause_end_idx])
+            clause_start_idx = clause_end_idx
+
+    clauses.append(sent[clause_start_idx:])
+
+    
+
+vocab_ext = VocabExtractor()
+
+for clause in clauses:
+    print(clause)
+    
+    print(vocab_ext.extract_verbs(clause))
+
