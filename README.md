7c16c77a4aff5279349478fb085792ddca4b58fd
Faizan E Mustafa <femustafa@lab.local>
2023-09-15 18:16:19 +0200

    * update readme

--- README.md
+++ README.md
@@ -1,21 +1,51 @@
-# German Vocabulary Extractor
-Extract german vocabulary from text
+# DEExtract
+Extract german vocabulary from text and use list of verbs to generate sample texts on specific topic at a particular language proficiency level.
 
-![website demo](dump/images/website_demo.PNG)
+![website demo](dump/images/vocab_extraction_demo.PNG)
+![website demo](dump/images/retriever_demo.PNG)
 
-
-
-## Run scripts
-
-* Install packages.
+# Package Installation
 
 ```bash
 pip install -r requirements.txt
 ```
 
-* Run the streamlit app.
+## Data Preparation:
+Note: The data is already uploaded. Skip to [next](#Dashboard) section to start the dashboard
+
+`dataset_prep.py`: Filter data from MLSUM dataset using topic names and use `VocabExtractor` to extract vocabulary. Further details are mentioned in 3.1 of the paper. The output is stored in `data/filtered_topics_{dataset_split_name}.json`
+
+`chatgpt.py`: Generated synthetic using the extracted vocab from `data/filtered_topics_train.json` and store the result as `data/gpt_dataset_{cefr_level}.json`
+
+`ìnverted_index.py`: Split the `data/gpt_dataset_{cefr_level}.json` by topic also `dataset_{topic}_{cefr_level}.json` and create inverted index `inverted_index{topic}_{cefr_level}.json`
+
+## Dashboard
+`lang_app.py`: Start the dashboard usifn the following command
 
 ```bash
 streamlit run lang_app.py 
 ```
+
+### Utils: 
+
+`readability_score.py`: Calculate readability scores
+
+## Text Generation Using T5
+
+Note: This section is not part of the publication.
+
+Use the vocabulary and topic to create a sythetic paragraph. The model was develeped as an alternative for the ChatGPT. However, the model sometimes does not generate coherent text.
+
+`dataset_prep.py`: Prepare the dataset.
+
+`train.py`: Train the T5 model
+
+`predict.py`: Generate the text using some input topic and list of vocabulary. 
+
+![website demo](dump/images/t5_demo.PNG)
+
+
+
+
+
 
--- dataset_prep.py
+++ dataset_prep.py
@@ -8,6 +8,8 @@ from datasets import load_dataset, load_metric
 vocab_ext = VocabExtractor()
 nlp = spacy.load("de_core_news_sm")
 
+#############
+# T5 Data Prep using gnad10 datatset
 
 # raw_datasets = load_dataset("gnad10")
 # # import pdb; pdb.set_trace()
@@ -76,97 +78,97 @@ nlp = spacy.load("de_core_news_sm")
 
 
 ############################################
+# Data preparation for ChatGPT
+
+raw_datasets = load_dataset("mlsum", 'de')
+# ClassLabels = raw_datasets['train'].features["label"]
+for ds_name, ds in raw_datasets.items():
+    print(ds_name)
+
+    texts = ds["text"]
+    labels = ds["topic"]
+
+    # label_names = ClassLabels.int2str(labels)
+    with open(f"filtered_topics_{ds_name}.json" , "w", encoding="utf-8") as f:
+        for text, label_name in tqdm(zip(texts, labels), total=len(texts)):
+            # {'bildung', 'reisefuehrer', 'reise',  'wirtschaft', 'politik', 'geld',  'sport', 'digital', 'stil',  'kultur', 'karriere', 'auto'}
+            if label_name not in { 'politik', 'wirtschaft'}:
+                continue
+            if len(text.split())>200:
+                continue
+            doc = nlp(text)
+            sections = [text]
+
+            if len(list(doc.sents)) > 4:
+
+                sections = []
+                section = []
+                for i, sent in enumerate(doc.sents):
 
 
-# raw_datasets = load_dataset("mlsum", 'de')
-# # ClassLabels = raw_datasets['train'].features["label"]
-# for ds_name, ds in raw_datasets.items():
-#     print(ds_name)
+                    if i !=0 and i % 4 == 0:
+                        sections.append(" ".join(section))
+                        # import pdb; pdb.set_trace()
+                        section = []
 
-#     texts = ds["text"]
-#     labels = ds["topic"]
+                    section.append(sent.text)
 
-#     # label_names = ClassLabels.int2str(labels)
-#     with open(f"filtered_topics_{ds_name}.json" , "w", encoding="utf-8") as f:
-#         for text, label_name in tqdm(zip(texts, labels), total=len(texts)):
-#             # {'bildung', 'reisefuehrer', 'reise',  'wirtschaft', 'politik', 'geld',  'sport', 'digital', 'stil',  'kultur', 'karriere', 'auto'}
-#             if label_name not in { 'politik', 'wirtschaft'}:
-#                 continue
-#             if len(text.split())>200:
-#                 continue
-#             doc = nlp(text)
-#             sections = [text]
+                if section:
+                    sections.append(" ".join(section))
 
-#             if len(list(doc.sents)) > 4:
+            for section in sections:
+                sec = nlp(section)
+                extracted_vocab = []
+                for sentence in sec.sents:
+                    extracted_vocab.extend(vocab_ext.extract_vocab(sentence))
 
-#                 sections = []
-#                 section = []
-#                 for i, sent in enumerate(doc.sents):
+                unique_vocab = []
+                for i in extracted_vocab:
+                    if i not in unique_vocab:
+                        unique_vocab.append(i)
 
+                token_status_threhold = 4.5
+                # import pdb; pdb.set_trace()
+                unique_vocab = [i for i in unique_vocab if vocab_ext.check_token_status(i, token_status_threhold) == "easy"]
 
-#                     if i !=0 and i % 4 == 0:
-#                         sections.append(" ".join(section))
-#                         # import pdb; pdb.set_trace()
-#                         section = []
+                if len(unique_vocab) < 3  or len(unique_vocab) > 5:
+                    continue
 
-#                     section.append(sent.text)
-
-#                 if section:
-#                     sections.append(" ".join(section))
-
-#             for section in sections:
-#                 sec = nlp(section)
-#                 extracted_vocab = []
-#                 for sentence in sec.sents:
-#                     extracted_vocab.extend(vocab_ext.extract_vocab(sentence))
-
-#                 unique_vocab = []
-#                 for i in extracted_vocab:
-#                     if i not in unique_vocab:
-#                         unique_vocab.append(i)
-
-#                 token_status_threhold = 4.5
-#                 # import pdb; pdb.set_trace()
-#                 unique_vocab = [i for i in unique_vocab if vocab_ext.check_token_status(i, token_status_threhold) == "easy"]
-
-#                 if len(unique_vocab) < 3  or len(unique_vocab) > 5:
-#                     continue
-
-#                 # unique_vocab = " </s> ".join(unique_vocab)
-#                 # # document = label_name + " <extra_id_0> " +  unique_vocab + " <extra_id_1>"
-#                 # document = label_name + " </s> " +  unique_vocab + " </s>"
+                # unique_vocab = " </s> ".join(unique_vocab)
+                # # document = label_name + " <extra_id_0> " +  unique_vocab + " <extra_id_1>"
+                # document = label_name + " </s> " +  unique_vocab + " </s>"
 
 
-#                 # json.dump({"document": document, "summary": text}, f)
-#                 # f.write('\n')
+                # json.dump({"document": document, "summary": text}, f)
+                # f.write('\n')
 
 
-#                 json.dump({"topic": label_name, "unique_vocab": unique_vocab, "section": section }, f)
-#                 f.write('\n')
+                json.dump({"topic": label_name, "unique_vocab": unique_vocab, "section": section }, f)
+                f.write('\n')
 
-############################################
 
 
 ###########################################
-import config
+# Data prep for t5 model 
+# import config
 
-data = []
-with open(config.DATA_ROOT / "gpt_dataset_c2.json", "r") as f:
-    for line in f:
-        data.append(json.loads(line))
+# data = []
+# with open(config.DATA_ROOT / "gpt_dataset_c2.json", "r") as f:
+#     for line in f:
+#         data.append(json.loads(line))
 
 
-with open(config.DATA_ROOT / "train.json", "w", encoding="utf-8") as f:
-    for entry in data:
-        unique_vocab = entry["vocab"]
+# with open(config.DATA_ROOT / "train.json", "w", encoding="utf-8") as f:
+#     for entry in data:
+#         unique_vocab = entry["vocab"]
 
-        unique_vocab_str = " ".join(
-            [f"{e} <extra_id_{i+1}>" for i, e in enumerate(entry["vocab"])]
-        )
+#         unique_vocab_str = " ".join(
+#             [f"{e} <extra_id_{i+1}>" for i, e in enumerate(entry["vocab"])]
+#         )
 
-        document = (
-            entry["topic"] + " <extra_id_0> " + unique_vocab_str
-        )  # + f" {original_text} <extra_id_{len(unique_vocab)+1}>"
+#         document = (
+#             entry["topic"] + " <extra_id_0> " + unique_vocab_str
+#         )  # + f" {original_text} <extra_id_{len(unique_vocab)+1}>"
 
-        json.dump({"document": document, "summary": entry["text"]}, f)
-        f.write("\n")
+#         json.dump({"document": document, "summary": entry["text"]}, f)
+#         f.write("\n")
